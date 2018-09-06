b202569102ca88b2d84cbfc9e996991cbaa8c638
Matt F <bassmattf@gmail.com>
2018-09-05 23:04:30 -0500

    All function prototypes required by SOW added

---
+++ Project1/Project1/Student.h
@@ -0,0 +1,76 @@
+/*******************************************************************
+*   File name: Student.h
+*   Author's name: Matt Fletcher
+*   Programming Assignment 1:  //TODO Program Name
+*   
+*   This program is entirely my own work
+*******************************************************************/
+#ifndef STUDENT_H
+#define STUDENT_H
+
+
+class Student
+{
+public:
+
+	//Default Constructor
+	Student();
+
+	//Second constructor
+	Student(int iID, char *mName, char *wName, char *hName);
+
+	//TODO Destructor function
+
+//////////////////////////
+	//Getters and Setters
+
+	//Get and set the student ID
+	int getStudentID();
+	void setStudentID(int iID);
+
+	//Get and set the student's name and wizarding family name. 
+	void getName(char *mName, char *wName);
+	void setName(char *mName, char *wName);
+
+	//- Get and set the student's house name. 
+	void getHouse(char *hName);
+	void setHouse(char *hName);
+
+	//Get and set a class name
+	void getClass(int idx, char *className);
+	void setClass(int idx, char *className);
+
+	//Get and Set a grade
+	void setGrade(int idx, int grade);
+	void getGrade(int idx, int &iGrade, char &cGrade); //Reference function that returns a letter and number grade TODO use hogwarts grading scale
+	void getGrade(int idx, int *iGrade, char *cGrade); //Pointer function that returns a letter and umber grade
+
+	//Print All student information
+	void printStudentInfo();
+
+
+
+
+private:
+	//Holds the student's ID number
+	int m_iStudentID;
+
+	//Char array for Magical name
+	char m_sMagicalName[64];
+
+	//Char Array for Family name
+	char m_sWizardFamilyName[64];
+
+	//Char array for house name
+	char m_sHouse[64];
+
+	//2D array of char for all class names
+	char m_sClasses[8][32];
+
+	//Array of ints to hold student's numerical grades
+	int m_iGrades[8];
+	
+};
+
+
+#endif
