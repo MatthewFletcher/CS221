8c17a9695838edda4720a9f80d6ef98f8cd35cc5
Matt F <bassmattf@gmail.com>
2018-09-06 00:03:26 -0500

    Added some testing for studentID functions

--- Project1/Project1/main.cpp
+++ Project1/Project1/main.cpp
@@ -1,8 +1,25 @@
+/*******************************************************************
+*   File name: main.cpp
+*   Author's name: Matt Fletcher
+*   Programming Assignment 1:  //TODO Program Name
+*   
+*   This program is entirely my own work
+*******************************************************************/
+
 #include <iostream>
+#include "Student.h"
+
 using namespace std;
 
 int main()
 {
+
+	Student s1;
+
+	s1.setStudentID(10);
+
+	
+
 	cout << "Hello, WOrld" << endl;
 	return 0;
 
