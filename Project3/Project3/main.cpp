3e2bfd5c4d0e83ae3c10b290e2365fb40ba23682
Fletcher <bassmattf@gmail.com>
2018-10-21 12:58:34 -0500

    Testing functions added

--- Project3/Project3/main.cpp
+++ Project3/Project3/main.cpp
@@ -1,11 +1,38 @@
 #include <iostream>
+#include "Library.h"
 
 using namespace std;
 
 int main()
 {
-	cout << "HEllo, World" << endl;
+	Library *lib = new Library();
+	lib->buildLibrary("testDB.txt");
+	
+	// // TESTING Print Library
+	//lib->printLibrary();
 
-	getchar();
+	// //Testing Getbookbynumber
+	// Book *bk;
+	// bk= lib->getBookByNumber(12485);
+	// cout << bk->Title << "\t" << endl;
+
+
+
+	int bookstoremove[] = {12385, 12365, 12405, 12355,12375,12395,12415};
+
+
+	Book *bk;
+
+	for (int i = 0; i <=6; ++i)
+	{
+		bk = lib->removeBook(bookstoremove[i]);
+		
+		if (bk!= NULL)
+		cout << "Removed Book " << bookstoremove[i] << ": " << bk->Title << endl;
+
+		cout << endl;
+	}
+
+
 	return 0;
 }
